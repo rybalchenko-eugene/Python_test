4c4044aa95003bc10e7edaa16ed0d065c5d4ff57
Reebak <reebak@rambler.ru>
2022-05-21 20:17:18 +0300

    changing base fjr variable below 10

--- convers.py
+++ convers.py
@@ -1,7 +1,9 @@
 print ("input a decimal number: ")
 num10 = int(input())
+print ("input a base of conversion (from 2 to 9): ")
+basenum = int (input ())
 num2 = ''
 while num10 > 0:
-    num2 = str(num10 % 2) + num2
-    num10 = num10 // 2
+    num2 = str(num10 % basenum) + num2
+    num10 = num10 // basenum
 print (num2)
