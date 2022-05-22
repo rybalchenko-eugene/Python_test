47a93bbbdc1bdd6dd8752ac6ceb402be96fe7395
Reebak <reebak@rambler.ru>
2022-05-22 12:57:54 +0300

    добавил перевод до 16

--- convers.py
+++ convers.py
@@ -3,7 +3,10 @@ num10 = int(input())
 print ("input a base of conversion (from 2 to 9): ")
 basenum = int (input ())
 num2 = ''
+dic = {10 : 'A', 11 : 'B', 12 : 'C', 13 : 'D', 14 : 'E', 15 : 'F'}
 while num10 > 0:
-    num2 = str(num10 % basenum) + num2
+    if (num10 % basenum) < 10:
+        num2 = str(num10 % basenum) + num2
+    else: num2 = dic[num10 % basenum] + num2
     num10 = num10 // basenum
 print (num2)
