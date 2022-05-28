d4839133bfd2e1af746bc9ff952adfcf80aa9fa4
rybalchenko-eugene <106405926+rybalchenko-eugene@users.noreply.github.com>
2022-05-28 13:14:07 +0300

    added file conversion

---
+++ convers.py
@@ -0,0 +1,12 @@
+print ("input a decimal number: ")
+num10 = int(input())
+print ("input a base of conversion (from 2 to 16): ")
+basenum = int (input ())
+num2 = ''
+dic = {10 : 'A', 11 : 'B', 12 : 'C', 13 : 'D', 14 : 'E', 15 : 'F'}
+while num10 > 0:
+    if (num10 % basenum) < 10:
+        num2 = str(num10 % basenum) + num2
+    else: num2 = dic[num10 % basenum] + num2
+    num10 = num10 // basenum
+print (num2)
